60be351c6518351bb15dcf474568728eaee12023
Austin Dang <austindang415@gmail.com>
2022-05-09 02:28:17 -0700

    404

--- frontend/src/components/404/index.js
+++ frontend/src/components/404/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import './404.css'
+
 const ErrorPage = () => {
  return (
   <div>
-   <h1> Page Not Found</h1>
+   <h1 className='error'> 404 </h1>
+   <h1 className='message'> Page Not Found</h1>
   </div>
  )
 }
